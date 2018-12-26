2a3439e6b1f59bab712dfe552cd882215d2310f9
M7mdAmr3 <43708078+M7mdAmr3@users.noreply.github.com>
2018-12-26 06:35:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,28 +16,28 @@ client.on('ready', function() {
 
 
 
-const developers = ["398558318030749739","486697035718852619",""]
-const adminprefix = ".";
+const developers = ["398558318030749739","486697035718852619","527006451269369877"]
+const adminprefix = "ok";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'ply')) {
+  if (message.content.startsWith(adminprefix + 'p')) {
     client.user.setGame(argresult);
       message.channel.send(`**dun  ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leaveserver")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'wat')) {
+  if (message.content.startsWith(adminprefix + 'w')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**dun ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
+  if (message.content.startsWith(adminprefix + 'l')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**  dun ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'stream')) {
+  if (message.content.startsWith(adminprefix + 's')) {
     client.user.setGame(argresult, "https://www.twitch.tv/xnxc");
       message.channel.send(`**DONE   ${argresult}**`)
   }
